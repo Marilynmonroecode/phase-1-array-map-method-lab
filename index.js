114ec38eee6208045fbc98de348f545be0516c48
marilyn.musyimi <marilyn.musyimi@student.moringaschool.com>
2024-10-12 09:29:10 +0300

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// Function to convert tutorial names to title case
+function titleCased() {
+  return tutorials.map(tutorial => {
+      // Split the tutorial into words
+      const words = tutorial.split(' ');
+
+      // Capitalize the first letter of each word
+      const capitalizedWords = words.map(word => {
+          // Capitalize the first letter and add the rest of the word
+          return word.charAt(0).toUpperCase() + word.slice(1);
+      });
+
+      // Join the capitalized words back into a string
+      return capitalizedWords.join(' ');
+  });
 }
